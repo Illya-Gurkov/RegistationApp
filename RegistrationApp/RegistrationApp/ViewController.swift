5c60c61675b0ddb378827040f4ce707cb47a5f9d
Illya-Gurkov <109282408+Illya-Gurkov@users.noreply.github.com>
2022-07-29 14:39:47 +0300

    New project created

---
+++ RegistrationApp/RegistrationApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RegistrationApp
+//
+//  Created by Illya Gurkov on 29.07.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
