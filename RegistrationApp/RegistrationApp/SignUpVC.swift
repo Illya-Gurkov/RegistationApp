44b785a411d8fa455e7600a304b8a9ed095b94ca
Illya-Gurkov <109282408+Illya-Gurkov@users.noreply.github.com>
2022-08-02 22:12:29 +0300

    commitReg

--- RegistrationApp/RegistrationApp/SignUpVC.swift
+++ RegistrationApp/RegistrationApp/SignUpVC.swift
@@ -27,21 +27,50 @@ final class SignUpVC: UIViewController {
     
     @IBOutlet private weak var signUpBtn: UIButton!
     
-    
+    // MARK: - Properties
+    private var isValidEmail = false { didSet { updateSignUp()} }
+    private var isConfPass = false { didSet { updateSignUp()} }
+    private var passwordStrength: PasswordStrength = .veryWeak { didSet { updateSignUp()} }
+
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        hideKeyboardWhenTappedAround()
     }
     // MARK: - Func-s
     
     @IBAction func emailTFAction(_ sender: UITextField) {
+        if let email = sender.text,
+           !email.isEmpty,
+           VerificationService.isValidEmail(email: email){
+            isValidEmail = true
+        } else {
+            isValidEmail = false
+        }
+        errorEmailLbl.isHidden = isValidEmail
     }
     
     @IBAction func passTFAction(_ sender: UITextField) {
+        if let passText = sender.text,
+           !passText.isEmpty{
+            passwordStrength = VerificationService.isValidPassword(pass: passText)
+        }
+        errorPassLbl.isHidden = passwordStrength != .veryWeak
+        sutupView()
     }
     
     
     @IBAction func confPassTFAction(_ sender: UITextField) {
+        if let confPassText = sender.text,
+           !confPassText.isEmpty,
+           let passText = passwordTF.text,
+           !passText.isEmpty {
+            isConfPass = VerificationService.isPassCofirm(pass1: passText, pass2: confPassText)
+        } else {
+            isConfPass = false
+        }
+        errorConfPassLbl.isHidden = isConfPass
     }
     
     @IBAction func signInBtnAction() {
@@ -51,11 +80,23 @@ final class SignUpVC: UIViewController {
     
     @IBAction func SignUpActionBtn() {
     }
+    // MARK: - Fun-s
+    
+    private func sutupView() {
+        indicatorsViews.enumerated().forEach { index, view in
+            if index <= (passwordStrength.rawValue - 1) {
+                view.alpha = 1
+            }  else {
+                view.alpha = 0.1
+            }
+        }
+    }
+    
+    private func updateSignUp() {
+        signUpBtn.isEnabled = isValidEmail && isConfPass && passwordStrength != .veryWeak
+    }
     
     
-    
-    
-
     /*
     // MARK: - Navigation
 
@@ -67,3 +108,4 @@ final class SignUpVC: UIViewController {
     */
 
 }
+
